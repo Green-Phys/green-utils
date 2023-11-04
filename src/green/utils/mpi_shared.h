8bd07c8787111650653dffc9593e982a74ec733a
iskakoff <sir.iskakoff@gmail.com>
2023-11-04 05:41:04 -0400

    custom constructor on shared wrapper

--- src/green/utils/mpi_shared.h
+++ src/green/utils/mpi_shared.h
@@ -11,43 +11,46 @@ namespace green::utils {
   template <typename Shared>
   class shared_object {
   private:
-    Shared& _object;
-    size_t  _size;
-    size_t  _local_size;
-    MPI_Win _win;
+    Shared                       _object;
+    size_t                       _size;
+    typename Shared::value_type* _ref;
+    size_t                       _local_size;
+    MPI_Win                      _win;
+
+    void                         assign_ptr() {
+      _local_size = _size / mpi_context::context().node_size;
+      _local_size += ((_size % mpi_context::context().node_size) > mpi_context::context().node_rank) ? 1 : 0;
+      MPI_Aint l_size = _local_size;
+      MPI_Aint g_size = _local_size;
+      setup_mpi_shared_memory(&_ref, _local_size, g_size, _win, mpi_context::context());
+      _object.set_ref(_ref);
+    }
 
   public:
-    shared_object(Shared& obj) : _object(obj), _size(obj.size()) {
-      _local_size = _size / mpi_context::context().node_size;
-      _local_size += ((_size % mpi_context::context().node_size) > mpi_context::context().node_rank) ? 1 : 0;
-      MPI_Aint                     l_size = _local_size;
-      MPI_Aint                     g_size = _local_size;
-      typename Shared::value_type* ref;
-      setup_mpi_shared_memory(&ref, _local_size, g_size, _win, mpi_context::context());
-      _object.set_ref(ref);
+    template <typename... Args>
+    shared_object(size_t s1, Args... args) : _object(nullptr, s1, size_t(args)...), _size(_object.size()) {
+      assign_ptr();
     }
 
-    shared_object(Shared&& obj) : _object(obj), _size(obj.size()) {
-      _local_size = _size / mpi_context::context().node_size;
-      _local_size += ((_size % mpi_context::context().node_size) > mpi_context::context().node_rank) ? 1 : 0;
-      MPI_Aint                     l_size = _local_size;
-      MPI_Aint                     g_size = _local_size;
-      typename Shared::value_type* ref;
-      setup_mpi_shared_memory(&ref, _local_size, g_size, _win, mpi_context::context());
-      _object.set_ref(ref);
-    }
+    shared_object(Shared& obj) : _object(obj), _size(obj.size()) { assign_ptr(); }
+
+    shared_object(Shared&& obj) : _object(obj), _size(obj.size()) { assign_ptr(); }
 
     shared_object(const shared_object& rhs) = delete;
-    shared_object(shared_object&& rhs) : _object(rhs._object), _size(rhs._size), _local_size(rhs._local_size), _win(rhs._win) {
+    shared_object(shared_object&& rhs) :
+        _object(rhs._object), _size(rhs._size), _ref(rhs._ref), _local_size(rhs._local_size), _win(rhs._win) {
       rhs._win = MPI_WIN_NULL;
+      rhs._ref = nullptr;
     }
 
     shared_object& operator=(shared_object&& rhs) {
       _object     = rhs._object;
       _size       = rhs._size;
+      _ref        = rhs._ref;
       _local_size = rhs._local_size;
       _win        = rhs._win;
       rhs._win    = MPI_WIN_NULL;
+      rhs._ref    = nullptr;
       return *this;
     }
 
