49e41630d513095d737ac225e346aa29e20470e5
Sergei Iskakov <sir.iskakoff@gmail.com>
2024-03-23 13:06:01 -0400

    improve coverage

--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -1,6 +1,22 @@
 /*
- * Copyright (c) 2023 University of Michigan.
+ * Copyright (c) 2024 University of Michigan
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the “Software”), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 #include <catch2/catch_approx.hpp>
@@ -67,14 +83,5 @@ TEST_CASE("Timing") {
     statistic.start("TEST");
     REQUIRE(statistic.event("UNKNOWN2").parent == &statistic.event("TEST"));
     statistic.end();
-  }
-
-  SECTION("Test Context without MPI_Init") {
-    REQUIRE(green::utils::context.global_rank == 0);
-    REQUIRE(green::utils::context.global_size == 1);
-    REQUIRE(green::utils::context.node_rank == 0);
-    REQUIRE(green::utils::context.node_size == 1);
-    REQUIRE(green::utils::context.internode_rank == 0);
-    REQUIRE(green::utils::context.internode_size == 1);
   }
 }
